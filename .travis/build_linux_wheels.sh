5e4c655fee88560bd1272666b7fd50246277089f
Alex Grönholm <alex.gronholm@nextday.fi>
2017-10-11 01:40:09 +0300

    Simplified the config

--- .travis/build_linux_wheels.sh
+++ .travis/build_linux_wheels.sh
@@ -4,6 +4,6 @@ set -e
 
 for PYVER in "cp27-cp27mu" "cp34-cp34m" "cp35-cp35m" "cp36-cp36m"; do
   PYBIN="/opt/python/${PYVER}/bin"
-  "${PYBIN}/pip" install Cython wheel
+  "${PYBIN}/pip" install Cython
   "${PYBIN}/python" setup.py bdist_wheel
 done
--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
     name='sample_c_extension',
-    setup_requires=['Cython'],
     ext_modules=cythonize('hello.pyx')
 )

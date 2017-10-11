2fd57b6d4acd91434ed7dc033d91053d9c152a37
Alex Grönholm <alex.gronholm@nextday.fi>
2017-10-11 15:06:44 +0300

    Run "auditwheel repair" to turn wheels into manylinux1 wheels

--- .travis/build_linux_wheels.sh
+++ .travis/build_linux_wheels.sh
@@ -7,3 +7,4 @@ for PYVER in "cp27-cp27mu" "cp34-cp34m" "cp35-cp35m" "cp36-cp36m"; do
   "${PYBIN}/pip" install Cython
   "${PYBIN}/python" setup.py bdist_wheel
 done
+find dist -name "*.whl" -exec auditwheel repair {} \;

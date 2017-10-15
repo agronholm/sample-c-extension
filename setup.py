5991eab427e6f4e30e9bf20f358e7e0db35ce660
Alex Grönholm <alex.gronholm@nextday.fi>
2017-10-15 13:58:53 +0300

    Added setuptools_scm to the packaging setup

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from Cython.Build import cythonize
 
 setup(
     name='sample_c_extension',
+    use_scm_version={
+        'version_scheme': 'post-release',
+        'local_scheme': 'dirty-tag'
+    },
     ext_modules=cythonize('hello.pyx'),
+    setup_requires=['setuptools_scm'],
     extras_require={
         'test': ['pytest']
     }
